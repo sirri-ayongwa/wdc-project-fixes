185c68c3b4bd16ad145bfacfe8f689d21eafd72c
Joseph <48150537+Josephbakulikira@users.noreply.github.com>
2024-05-25 22:20:32 +0200

    add design and convert to tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,30 @@
-import { useState } from "react";
-import "./App.css";
-import HomePage from "./pages/home";
-import NotfoundPage from "./pages/notfound";
-import { Routes, Route } from "react-router-dom";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Services from "./components/Services";
 
-function App() {
+const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
-      {/* <Routes>
-        <Route exact path="/" element={<HomePage/>}/>
-        <Route path="*" element={<NotfoundPage/>}/>
-      </Routes> */}
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
+      </div>
+
+      <ButtonGradient />
     </>
   );
-}
+};
 
 export default App;
