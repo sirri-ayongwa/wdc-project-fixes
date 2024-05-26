b6ca5f47a5d9b0766f11ece16bc6a2b2108720df
Joseph <48150537+Josephbakulikira@users.noreply.github.com>
2024-05-26 15:03:13 +0200

    fix routing and production condition

--- backend/server.js
+++ backend/server.js
@@ -81,17 +81,21 @@ app.use('/api/posts', postRoute);
 
 __dirname = path.resolve()
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+app.get('/', (req, res) => {
+  res.send('API is running....')
+})
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  )
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....')
-  })
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+//   )
+// } else {
+//   app.get('/', (req, res) => {
+//     res.send('API is running....')
+//   })
+// }
 
 
 //error middleware
