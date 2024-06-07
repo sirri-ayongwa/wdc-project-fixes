6b3d4b1d0d8aa5ebebb6f345b4bdd816987c86ff
Joseph <48150537+Josephbakulikira@users.noreply.github.com>
2024-06-07 14:04:46 +0200

    fix mails on website , onclick to onsubmit

--- frontend/src/components/contactForm.jsx
+++ frontend/src/components/contactForm.jsx
@@ -41,7 +41,7 @@ function ContactForm() {
       <div className="max-w-md mx-auto relative overflow-hidden z-10 bg-gray-800 p-8 rounded-lg shadow-md before:w-24 before:h-24 before:absolute before:bg-purple-600 before:rounded-full before:-z-10 before:blur-2xl after:w-32 after:h-32 after:absolute after:bg-sky-400 after:rounded-full after:-z-10 after:blur-xl after:top-24 after:-right-12">
         <h2 className="text-2xl font-bold text-white mb-6">Keep in Touch</h2>
 
-        <form ref={form} onClick={sendEmail} action="#">
+        <form ref={form} onSubmit={sendEmail} action="#">
           <div className="mb-4">
             <label
               className="block text-sm font-medium text-gray-300"
