8011e503c23c213b4a7987fdb29585edf0f862da
Joseph <48150537+Josephbakulikira@users.noreply.github.com>
2024-06-12 12:17:28 +0200

    update the website

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -12,6 +12,7 @@ import Notification from "./Notification";
 import CompanyLogos from "./CompanyLogos";
 import { MdTornado, MdCloud } from "react-icons/md";
 import { FaWater, FaWaveSquare } from "react-icons/fa";
+import GIF1 from '../assets/gif/2.webp'
 
 const Hero = () => {
   const parallaxRef = useRef(null);
@@ -24,6 +25,7 @@ const Hero = () => {
       customPaddings
       id="hero"
     >
+      <img className="absolute opacity-15 min-h-[400px]" width="100%"  src={GIF1} alt="world data"/>
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
@@ -38,6 +40,7 @@ const Hero = () => {
                 alt="Curve"
               />
             </span>
+            
           </h1>
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
             GDMAS is equipped with state-of-the-art technology that integrates
