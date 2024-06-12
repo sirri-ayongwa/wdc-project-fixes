ec1b7ad1bdc09027f214aa25917a5b628fad4444
Joseph <48150537+Josephbakulikira@users.noreply.github.com>
2024-06-12 14:00:42 +0200

    updating benefiting section

--- frontend/src/components/Benefits.jsx
+++ frontend/src/components/Benefits.jsx
@@ -37,9 +37,9 @@ const Benefits = () => {
                     alt={item.title}
                   /> */}
 
-                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
+                  <Link to="/blogs" className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
                     Explore more
-                  </p>
+                  </Link>
 
                   <Arrow />
                 </div>
