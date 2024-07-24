d0553c474137fcf9dd1ad53e53378e6a49ebf08a
Joseph <48150537+Josephbakulikira@users.noreply.github.com>
2024-07-24 12:53:33 +0200

    adding the addresses in the footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,9 +5,26 @@ import { socials } from "../constants";
 const Footer = () => {
   return (
     <Section crosses className="!px-0 !py-10">
+      <div className="container flex justify-center ">
+        <div>
+          <h3 className="text-color-1 text-center">Our addresses</h3>
+          <ul>
+            <li className="text-gray-400">
+              <b>New York</b> 1660 Madison Avenue apt 6B, New York, NY, 10029
+            </li>
+            <li className="text-gray-400">
+              <b>Austria</b> 5020, Wolf-Dietrich Strabe 31, Salzburg, Salzburg
+            </li>
+          </ul>
+        </div>
+      </div>
+      <br />
+      <br />
       <div className="container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col">
         <div className="flex justify-center">
-          <a href="mailto:office@worlddisastercenter.org">office@worlddisastercenter.org</a>
+          <a href="mailto:office@worlddisastercenter.org">
+            office@worlddisastercenter.org
+          </a>
         </div>
         <p className="caption text-n-4 lg:block">
           © {new Date().getFullYear()}. All rights reserved.
