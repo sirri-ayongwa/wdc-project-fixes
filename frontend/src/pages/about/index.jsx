e38e39be4e564f61720daf66b539907f42b98ac0
Joseph <48150537+Josephbakulikira@users.noreply.github.com>
2024-08-22 23:58:46 +0200

    adding a button do see the wdc catalog

--- frontend/src/pages/about/index.jsx
+++ frontend/src/pages/about/index.jsx
@@ -8,6 +8,7 @@ import WhoWeAre from "../../components/WhoWeAre";
 import OurMission from "../../components/OurMission";
 import OurVision from "../../components/OurVision";
 import OurValues from "../../components/ourValues";
+import Button from "../../components/Button";
 
 function AboutPage() {
   return (
@@ -15,6 +16,11 @@ function AboutPage() {
       <br />
       <br />
       <WhoWeAre />
+      <div className="flex justify-center">
+      <Button href="https://issuu.com/worlddisastercenter/docs/wdc_008_bleu_format">
+        Read more
+      </Button>
+      </div>
       <OurMission/>
       <OurVision/>
       <OurValues/>
