172f2961cf424f6355efdd6873592620e81648ed
kevinkirui2 <smoothdime@gmail.com>
2025-04-17 23:45:40 +0300

    setting urls for bypassing CORS

--- backend/server.ts
+++ backend/server.ts
@@ -19,7 +19,6 @@ import organizationRoutes from "./routes/organizationRoutes";
 import nodemailerRoutes from "./routes/nodemailerRoutes";
 
 // Initialize dotenv and Express
-//slight change
 dotenv.config();
 const app = express();
 app.set('trust proxy', 1);
@@ -30,7 +29,12 @@ app.use(express.json({ limit: "10mb" })); // Increased JSON payload limit
 app.use(express.urlencoded({ extended: true, limit: "10mb" })); // Increased URL-encoded payload limit
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: [
+    "http://localhost:5173", // Local development
+    "https://wdc-website-psi.vercel.app", // Production URL 1
+    "https://wdc-website-git-testing-world-disaster-center.vercel.app", // Production URL 2
+    "https://www.worlddisastercenter.org" // Production URL 3
+  ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type"]
 }));
