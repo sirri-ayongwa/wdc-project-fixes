6afd9028b22331e886b5ba199bbf692a2ca4f108
olivebishop <olivehendrilgen1@gmail.com>
2025-04-29 21:05:22 +0300

    fixes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,7 +8,6 @@ import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
 import MegaDropdown from "./dropdownItem";
 import SearchOverlay from "./SearchOverlay";
-
 const Header = () => {
   const location = useLocation();
   const [activeDropdown, setActiveDropdown] = useState(null);
